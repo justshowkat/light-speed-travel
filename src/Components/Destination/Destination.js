4ac7d921e132c3c94c5f98038c60d62d7bc10f48
mshowkat <md.showkat8@gmail.com>
2021-03-21 19:25:19 +0600

    google map updated

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Button, Card, Form, ListGroup, ListGroupItem } from "react-bootstrap";
+import Iframe from "react-iframe";
 import { useParams } from "react-router";
 import { bus, cars, bikes, trains} from "../FakeData/VehiclesData";
 import "./Destination.css";
-import GoogleMapComponent from "./GoogleMap/GoogleMap";
 import Vehicle from "./Vehicle/Vehicle";
 
 const Destination = () => {
@@ -59,10 +59,7 @@ const Destination = () => {
         </Card>) : ''}
       </div>
       <div className="column2">
-        <img src="./images/Map.png" alt="" srcset="" />
-        <div id='map-box'>
-          <GoogleMapComponent></GoogleMapComponent>
-        </div>
+      <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233668.38703692693!2d90.27923991057244!3d23.780573258035957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1616332672628!5m2!1sen!2sbd" className='custom-Map' allowfullscreen="" loading="lazy"></Iframe>
       </div>
     </div>
   );
